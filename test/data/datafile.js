e049de423919839b039550aff86ffabdc68b12fa
irinaintelycare <idedja@intelycare.com>
2023-05-30 11:44:10 +0200

    adding data file

---
+++ test/data/datafile.js
@@ -0,0 +1,6 @@
+export const Email = 'testautomation@mailinator.com';
+export const Password = 'Test@123';
+export const FirstName = 'Test';
+export const LastName = 'Test';
+export const MobileNumber = '9113137066';
+export const ZipCode = '02891';
